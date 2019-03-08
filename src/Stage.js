add25e2e2938a9a1dc9a53bf4b2925b1fd04f451
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2019-03-07 21:52:48 -0500

    faded in new stage on transition

--- src/Stage.js
+++ src/Stage.js
@@ -1,48 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Spring } from 'react-spring/renderprops';
 import StageHandler from './StageSelectionHandler';
 
 const Stage = ({ onSubmit, options, prompt }) => {
   return (
-    <>
-      <p className="prompt">{prompt}</p>
+    <Spring
+      from={{ opacity: 0 }}
+      to={{ opacity: 1 }}
+      config={{ duration: 500 }}
+    >
+      {props => (
+        <div style={props}>
+          <p className="prompt">{prompt}</p>
 
-      <StageHandler>
-        {({ onOptionToggled, selected, resetSelections }) => {
-          const optionToggles = options.map(option => {
-            const isSelected = selected[option];
+          <StageHandler>
+            {({ onOptionToggled, selected, resetSelections }) => {
+              const optionToggles = options.map(option => {
+                const isSelected = selected[option];
 
-            return (
-              <button
-                key={option}
-                type="button"
-                aria-pressed={isSelected ? 'true' : 'false'}
-                className={`option ${isSelected ? 'checked' : ''}`}
-                onClick={() => onOptionToggled(option, !isSelected)}
-              >
-                {option}
-              </button>
-            );
-          });
+                return (
+                  <button
+                    key={option}
+                    type="button"
+                    aria-pressed={isSelected ? 'true' : 'false'}
+                    className={`option ${isSelected ? 'checked' : ''}`}
+                    onClick={() => onOptionToggled(option, !isSelected)}
+                  >
+                    {option}
+                  </button>
+                );
+              });
 
-          return (
-            <>
-              <div className="options">{optionToggles}</div>
-              <button
-                className="cta"
-                type="button"
-                onClick={() => {
-                  onSubmit(Object.keys(selected));
-                  resetSelections();
-                }}
-              >
-                →
-              </button>
-            </>
-          );
-        }}
-      </StageHandler>
-    </>
+              return (
+                <>
+                  <div className="options">{optionToggles}</div>
+                  <button
+                    className="cta"
+                    type="button"
+                    onClick={() => {
+                      onSubmit(Object.keys(selected));
+                      resetSelections();
+                    }}
+                  >
+                    →
+                  </button>
+                </>
+              );
+            }}
+          </StageHandler>
+        </div>
+      )}
+    </Spring>
   );
 };
 
