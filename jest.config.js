94b67e5c26fa8795dd8eef0ad0f0b21c01169fff
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2019-03-11 00:18:01 -0400

    removed coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,4 @@ module.exports = {
     '^.+\\.(css|less|scss)$': '<rootDir>/test/styleMock.js',
   },
   collectCoverageFrom: ['**/src/**/*.js'],
-  coverageThreshold: {
-    global: {
-      statements: 17,
-      branches: 4,
-      lines: 17,
-      functions: 20,
-    },
-  },
 };
