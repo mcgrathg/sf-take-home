7ae626702beccb6a343470e3a8495f2de47ad90d
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2019-03-11 00:52:00 -0400

    added typeahead for jest

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
     '^.+\\.(css|less|scss)$': '<rootDir>/test/styleMock.js',
   },
   collectCoverageFrom: ['**/src/**/*.js'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
